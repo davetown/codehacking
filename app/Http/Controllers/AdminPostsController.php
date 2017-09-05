007f0d7473a81e8dbb0e18ae6eaa821ca607ff96
davetown <davetown@mac.com>
2017-09-05 13:58:48 +0100

    Posts working

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Http\Requests\PostsCreateRequest;
+use App\Photo;
+use App\Post;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminPostsController extends Controller
 {
@@ -14,7 +20,10 @@ class AdminPostsController extends Controller
     public function index()
     {
         //
-        return view('admin.posts.index');
+
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -25,7 +34,8 @@ class AdminPostsController extends Controller
     public function create()
     {
         //
-        return view('admin.posts.create');
+        $cats = Category::pluck('name','id')->all();
+        return view('admin.posts.create', compact('cats'));
     }
 
     /**
@@ -34,9 +44,32 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
         //
+
+
+
+        $input = $request->all();
+
+        $user = Auth::user();
+
+        if($file = $request->file('photo_id')){
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+        };
+
+       $user->posts()->create($input);
+
+       return redirect('admin/posts');
+
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,4 +5,39 @@
 
     <h1>Posts</h1>
 
+    <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+              <th>Photo</th>
+            <th>Posted By</th>
+            <th>Category</th>
+              <th>Title</th>
+              <th>Body</th>
+              <th>Created</th>
+              <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        @if($posts)
+
+            @foreach($posts as $post)
+          <tr>
+                <td>{{$post->id}}</td>
+              <td><img height = "50" src="{{$post->photo ? $post->photo->file : ''}}" alt="Photo of {{$post->name}}"></td>
+              <td>{{$post->user->name}}</td>
+                  <td>{{$post->category ? $post->category->name : 'No Category'}}</td>
+                <td>{{$post->title}}</td>
+                <td>{{$post->body}}</td>
+                  <td>{{$post->created_at->diffForHumans()}}</td>
+                  <td>{{$post->updated_at->diffForHumans()}}</td>
+          </tr>
+            @endforeach
+
+        @endif
+
+        </tbody>
+      </table>
+
     @endsection
